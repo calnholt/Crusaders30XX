35e46b75492b71c2ebc63944fbf84a8739fb39d7
Calvin Holt <>
2026-02-12 03:31:10 -0500

    card v2 plans and mocks

---
+++ ECS/Objects/Cards/Crusade.cs
@@ -0,0 +1,44 @@
+using System.Linq;
+using Crusaders30XX.ECS.Components;
+using Crusaders30XX.ECS.Core;
+using Crusaders30XX.ECS.Events;
+
+namespace Crusaders30XX.ECS.Objects.Cards
+{
+    public class Crusade : CardBase
+    {
+        private int ActionPointGain = 1;
+        private int AggressionGain = 3;
+        public Crusade()
+        {
+            CardId = "crusade";
+            Name = "Crusade";
+            Target = "Enemy";
+            Text = $"If this card is pledged, gain {ActionPointGain}AP and {AggressionGain} aggression.";
+            Cost = ["Black"];
+            Animation = "Attack";
+            Damage = 6;
+            Block = 2;
+
+            OnPlay = (entityManager, card) =>
+            {
+                var player = entityManager.GetEntity("Player");
+                var enemy = entityManager.GetEntity("Enemy");
+
+                if (card.GetComponent<Pledge>() != null)
+                {
+                    EventManager.Publish(new ModifyActionPointsEvent { Delta = ActionPointGain });
+                    EventManager.Publish(new ApplyPassiveEvent { Target = player, Type = AppliedPassiveType.Aggression, Delta = AggressionGain });
+                }
+
+                EventManager.Publish(new ModifyHpRequestEvent
+                {
+                    Source = player,
+                    Target = enemy,
+                    Delta = -GetDerivedDamage(entityManager, card),
+                    DamageType = ModifyTypeEnum.Attack
+                });
+            };
+        }
+    }
+}
