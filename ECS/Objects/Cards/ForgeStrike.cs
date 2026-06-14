ac827727373a4d7b154e67f345bb22f0b66b79d1
Calvin Holt <>
2026-06-14 19:58:40 -0400

    balance tweaks

---
+++ ECS/Objects/Cards/ForgeStrike.cs
@@ -0,0 +1,32 @@
+using Crusaders30XX.ECS.Core;
+using Crusaders30XX.ECS.Events;
+
+namespace Crusaders30XX.ECS.Objects.Cards
+{
+    public class ForgeStrike : CardBase
+    {
+        public ForgeStrike()
+        {
+            CardId = "forge_strike";
+            Name = "Forge Strike";
+            Target = "Enemy";
+            Animation = "Attack";
+            Damage = 7;
+            Cost = ["Any", "Any"];
+            Block = 2;
+            IsFreeAction = true;
+
+            OnPlay = (entityManager, card) =>
+            {
+                EventManager.Publish(new ModifyHpRequestEvent
+                {
+                    Source = entityManager.GetEntity("Player"),
+                    Target = entityManager.GetEntity("Enemy"),
+                    Delta = -GetDerivedDamage(entityManager, card),
+                    AttackCard = card,
+                    DamageType = ModifyTypeEnum.Attack
+                });
+            };
+        }
+    }
+}
